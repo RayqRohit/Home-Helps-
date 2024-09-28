8199326884664284343eb29e9383b85c353a2f1c
RayqRohit <rrajput1172@gmail.com>
2024-09-29 01:10:00 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+    const mongoUri = process.env.MONGODB_URI;
+    await mongoose.connect(mongoUri)
+        .then(() => console.log("connected to db"))
+        .catch(err => console.log(err));
+};
 
 
-    await mongoose.connect("mongodb+srv://rohit1172:mongo3301@cluster0.deox8.mongodb.net/food-del")
-        .then(() => console.log("connected to db"));
+// import mongoose from "mongoose";
 
-}
+// export const connectDB = async () => {
+
+
+//     await mongoose.connect("mongodb+srv://rohit1172:mongo3301@cluster0.deox8.mongodb.net/food-del")
+//         .then(() => console.log("connected to db"));
+
+// }
+
+
